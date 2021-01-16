1fee7738ad50b54e4dc30d564cab08ca577991da
Tius <asunaro1121@gmail.com>
2021-01-16 20:48:02 +0900

    Add DB insert process

--- receive.php
+++ receive.php
@@ -1,4 +1,5 @@
 <?php
+
 //Input
 $input_str = file_get_contents('php://input');
 $input_str = mb_convert_encoding($input_str, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
@@ -6,20 +7,32 @@ $input_str = mb_convert_encoding($input_str, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJI
 //Get array
 $arr = json_decode($input_str,true);
 
-//Access to each data
-foreach($arr as $data){
-    //Get each data
-    $sensor_id= "sensor_id　".$data['sensor_id']."\n"."<br>".PHP_EOL;
-    $point= "point　　".$data['point']."\n"."<br>".PHP_EOL;
-    $timestamp= "timestamp　　　".$data['timestamp']."\n"."<br>".PHP_EOL;
-    $count1= "count1　　".$data['count1']."\n"."<br>".PHP_EOL;
-    $count2= "count2　　".$data['count2']."\n"."<br>".PHP_EOL;
-   $real_array= array(
-      $sensor_id,$point,$timestamp,$count1,$count2,
-   );
+//Get each data
+$sensor_id = $arr['sensor_id'];
+$point     = $arr['point'];
+$timestamp = $arr['timestamp'];
+$count1    = $arr['count1'];
+$count2    = $arr['count2'];
 
-   //Output
-   echo implode('',$real_array);
-}
+//Access to DB
+$dsn = 'mysql:host=[hostname];dbname=[dbname];charset=utf8mb4';   
+$db_user = '[usrname]';
+$db_pass = '[password]';
+
+try {
+   $db = new PDO($dsn, $db_user, $db_pass);
+   echo "接続OK！";
+ } catch (PDOException $e) {
+   echo 'DB接続エラー！: ' . $e->getMessage();
+ }
+ 
+ //Insert new JSON data
+ //jsontable has (sensor_id, point, timestamp, count1, count2) columns
+ $sql = "INSERT INTO jsontable (sensor_id, point, timestamp, count1, count2) VALUES (:sensor_id, :point, :timestamp, :count1, :count2)";
+ $stmt = $db->prepare($sql);
+ $params = array(':sensor_id' => $sensor_id, ':point' => $point, ':timestamp' => $timestamp, ':count1' => $count1, ':count2' => $count2);
+ 
+ //Activate Insert
+ $stmt->execute($params);
 
 ?>
